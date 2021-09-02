5441206317dabce5380edf86b0cb36f247e59465
Manuel Muñoz Solera <mamuso@mamuso.net>
2021-09-01 23:09:50 -0700

    Initial grid

---
+++ pages/index.js
@@ -0,0 +1,138 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+import img41 from '../public/pics/41.jpg'
+import img40 from '../public/pics/40.jpg'
+import img39 from '../public/pics/39.jpg'
+import img38 from '../public/pics/38.jpg'
+import img37 from '../public/pics/37.jpg'
+import img36 from '../public/pics/36.jpg'
+import img35 from '../public/pics/35.jpg'
+import img34 from '../public/pics/34.jpg'
+import img33 from '../public/pics/33.jpg'
+import img32 from '../public/pics/32.jpg'
+import img31 from '../public/pics/31.jpg'
+import img30 from '../public/pics/30.jpg'
+import img29 from '../public/pics/29.jpg'
+import img28 from '../public/pics/28.jpg'
+import img27 from '../public/pics/27.jpg'
+import img26 from '../public/pics/26.jpg'
+import img25 from '../public/pics/25.jpg'
+import img24 from '../public/pics/24.jpg'
+import img23 from '../public/pics/23.jpg'
+import img22 from '../public/pics/22.jpg'
+import img21 from '../public/pics/21.jpg'
+import img20 from '../public/pics/20.jpg'
+import img19 from '../public/pics/19.jpg'
+import img18 from '../public/pics/18.jpg'
+import img17 from '../public/pics/17.jpg'
+import img16 from '../public/pics/16.jpg'
+import img15 from '../public/pics/15.jpg'
+import img14 from '../public/pics/14.jpg'
+import img13 from '../public/pics/13.jpg'
+import img12 from '../public/pics/12.jpg'
+import img11 from '../public/pics/11.jpg'
+import img10 from '../public/pics/10.jpg'
+import img9 from '../public/pics/9.jpg'
+import img8 from '../public/pics/8.jpg'
+import img7 from '../public/pics/7.jpg'
+import img6 from '../public/pics/6.jpg'
+import img5 from '../public/pics/5.jpg'
+import img4 from '../public/pics/4.jpg'
+import img3 from '../public/pics/3.jpg'
+import img2 from '../public/pics/2.jpg'
+import img1 from '../public/pics/1.jpg'
+import img0 from '../public/pics/0.jpg'
+
+export default function Home() {
+  return (
+    <>
+      <section className={styles.section}>
+        <Image src={img41} className={styles.sectionImg} />
+
+        <Image src={img40} className={styles.sectionImg} />
+
+        <Image src={img39} className={styles.sectionImg} />
+
+        <Image src={img38} className={styles.sectionImg} />
+
+        <Image src={img37} className={styles.sectionImg} />
+
+        <Image src={img36} className={styles.sectionImg} />
+
+        <Image src={img35} className={styles.sectionImg} />
+
+        <Image src={img34} className={styles.sectionImg} />
+
+        <Image src={img33} className={styles.sectionImg} />
+
+        <Image src={img32} className={styles.sectionImg} />
+
+        <Image src={img31} className={styles.sectionImg} />
+
+        <Image src={img30} className={styles.sectionImg} />
+
+        <Image src={img29} className={styles.sectionImg} />
+
+        <Image src={img28} className={styles.sectionImg} />
+
+        <Image src={img27} className={styles.sectionImg} />
+
+        <Image src={img26} className={styles.sectionImg} />
+
+        <Image src={img25} className={styles.sectionImg} />
+
+        <Image src={img24} className={styles.sectionImg} />
+
+        <Image src={img23} className={styles.sectionImg} />
+
+        <Image src={img22} className={styles.sectionImg} />
+
+        <Image src={img21} className={styles.sectionImg} />
+
+        <Image src={img20} className={styles.sectionImg} />
+
+        <Image src={img19} className={styles.sectionImg} />
+
+        <Image src={img18} className={styles.sectionImg} />
+
+        <Image src={img17} className={styles.sectionImg} />
+
+        <Image src={img16} className={styles.sectionImg} />
+
+        <Image src={img15} className={styles.sectionImg} />
+
+        <Image src={img14} className={styles.sectionImg} />
+
+        <Image src={img13} className={styles.sectionImg} />
+
+        <Image src={img12} className={styles.sectionImg} />
+
+        <Image src={img11} className={styles.sectionImg} />
+
+        <Image src={img10} className={styles.sectionImg} />
+
+        <Image src={img9} className={styles.sectionImg} />
+
+        <Image src={img8} className={styles.sectionImg} />
+
+        <Image src={img7} className={styles.sectionImg} />
+
+        <Image src={img6} className={styles.sectionImg} />
+
+        <Image src={img5} className={styles.sectionImg} />
+
+        <Image src={img4} className={styles.sectionImg} />
+
+        <Image src={img3} className={styles.sectionImg} />
+
+        <Image src={img2} className={styles.sectionImg} />
+
+        <Image src={img1} className={styles.sectionImg} />
+
+        <Image src={img0} className={styles.sectionImg} />
+      </section>
+    </>
+  )
+}
